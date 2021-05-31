64254a9f2b8c806b0a1f86655f662d2c6f33ccf4
Sam Baas <samuelpetrus@gmail.com>
2021-05-31 12:39:29 +0200

    added script to maintain certain order of predefined container items, even when LayoutGroups are doing their ordering

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/ForcedOrder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcedOrder : MonoBehaviour
+{
+	[SerializeField]
+	private Transform[] orderOfItems;
+	
+    void OnTransformChildrenChanged(){
+		for (int i = 0; i < orderOfItems.Length; i++)
+		{
+			orderOfItems[i].SetSiblingIndex(i);
+		}
+	}
+}
